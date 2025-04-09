8153ff61a75cf1fa6891b09d86bbcac88b526e11
hamze <123456789+odin@users.noreply.github.com>
2025-04-09 09:01:25 +0300

    stll trying to connect this shit

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+module.exports = function(eleventyConfig) {
+    // Copy styles
+    eleventyConfig.addPassthroughCopy("src/styles");
+  
+    // Create blog collection
+    eleventyConfig.addCollection("blog", function(collectionApi) {
+      return collectionApi.getFilteredByGlob("src/blog/*.md");
+    });
+  
+    // Add date filter (NEW)
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+      return new Date(dateObj).toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      });
+    });
+  
+    // Set template engines (NEW)
+    return {
+      markdownTemplateEngine: "njk",
+      htmlTemplateEngine: "njk",
+      templateFormats: ["html", "njk", "md"],
+      dir: {
+        input: "src",
+        includes: "_includes",
+        output: "_site"
+      }
+    };
+  };
