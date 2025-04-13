5f25e223a15b1d2b9fdb2768742a8851aa5487b6
hamze <123456789+odin@users.noreply.github.com>
2025-04-13 16:43:06 +0300

    final changes

--- _site/research/js/loader-header.js
+++ _site/research/js/loader-header.js
@@ -7,6 +7,20 @@ fetch('../research/shared/header.html')
   })
   .then(data => {
     document.getElementById('header-placeholder').innerHTML = data;
+
+    const menuToggle = document.querySelector('.menu-toggle');
+        const navLinks = document.querySelector('.nav-links');
+
+        menuToggle.addEventListener('click', function () {
+            navLinks.classList.toggle('active');
+        });
+
+        document.getElementById('researchDropdown').addEventListener('change', function () {
+            if (this.value) {
+                window.location.href = this.value;
+            }
+        });
+    
   })
   .catch(error => {
     console.error('Error loading header:', error);
